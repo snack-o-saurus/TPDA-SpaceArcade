0800db1d8037c465f4ca9ad8f8045d2d86681306
xXDjinTonicXx <103107608+xXDjinTonicXx@users.noreply.github.com>
2022-06-15 12:33:22 +0200

    Add files via upload

--- Space-InvaderV1.1/Classes.h
+++ Space-InvaderV1.1/Classes.h
@@ -1,9 +1,6 @@
 #ifndef CLASSES_H
 #define CLASSES_H
-#include <TMC2208Stepper.h>
-//#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
-#include <ezButton.h>
+#include "libraries.h"
 #include "Variable.h"
 
 #define SWPIN 2
@@ -14,25 +11,26 @@
 #define STEP_PIN_PAN  10        // Step on rising edge
 #define EN_PIN_PAN    13         // LOW = enable driver  / HIGH = disable driver
 #define DIR_PIN_PAN   11
-#define STEP_PIN_TILT  10        // Step on rising edge
-#define EN_PIN_TILT    9        // LOW = enable driver  / HIGH = disable driver
-#define DIR_PIN_TILT   8
+
+#define STEP_PIN_TILT  6        // Step on rising edge
+#define EN_PIN_TILT    7        // LOW = enable driver  / HIGH = disable driver
+#define DIR_PIN_TILT   5
+#define SERVO_PIN      9
+#define BUTTON_PIN     4
 
 
-ezButton button(SWPIN);
+ezButton button(BUTTON_PIN);
 TMC2208Stepper driver = TMC2208Stepper(&Serial);  // Create driver and use
-//LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27
+Servo servo; 
 
 class Gameobject {
 
   private:
-    String _id;
     byte _control;
-    int _score;
-
-
-
   public:
+    int _score;
+    String _id;
     byte _Dir;
     int _xPos;
     int _yPos;
@@ -56,30 +54,7 @@ class Gameobject {
 
     //Setter
     void setId(String id) {
-      _id = id;
-      /*
-      for (byte z = 0; z <= 8; z++) {
-        lcd.print( tempId + arrayLetters[i]);
-        for (i = 0; i <= 25 && _Dir == DOWN; i++) {
-          lcd.setCursor(1, 3);                            // Eingabeart ist noch nicht klar!!(vielleicht Buchstaben A-Z pro Zeichen durchlaufen und mit Button bestätigen und zur nächsten Stelle springen
-
-          delay(500);
-          if (button.isPressed()) {
-            lcd.clear();
-            j++;
-            lcd.setCursor(1, 3);
-            tempName[j] = arrayLetters[i];
-            lcd.print( tempId + arrayLetters[i]);
-            // Einzelne Buchstaben des Arrays in id übertragen in einen String
-          }
-          return  j;
-          break;
-        }
-        tempId = tempId + tempName[z];
-        
-      }
-      */
-      _id = tempId;
+      _id = id;   
     }
     void setControl(byte control) {
       _control = control;
@@ -164,7 +139,6 @@ class Gameobject {
           Serial.println("LEFT");
           digitalWrite(DIR_PIN_PAN, HIGH);
           analogWrite(STEP_PIN_PAN, 127);
-          //digitalWrite(STEP_PIN_PAN, HIGH);
           delay(50);
           break;
 
@@ -177,6 +151,7 @@ class Gameobject {
           break;
 
         case UP:
+        Serial.println("UP");
           digitalWrite(DIR_PIN_TILT, HIGH);
           analogWrite(STEP_PIN_TILT, 127);
           //digitalWrite(STEP_PIN_TILT, HIGH);
@@ -184,6 +159,7 @@ class Gameobject {
           break;
           
         case DOWN:
+        Serial.println("DOWN");
           digitalWrite(DIR_PIN_TILT, LOW);
           analogWrite(STEP_PIN_TILT, 127);
           //digitalWrite(STEP_PIN_TILT, HIGH);
@@ -191,11 +167,16 @@ class Gameobject {
           break;
           
         default:
+        //Serial.println("STOP");
           digitalWrite(STEP_PIN_TILT, LOW);
           digitalWrite(STEP_PIN_PAN, LOW);
           break;
       }
     }
+
+
+
+    
 };
 
 #endif
--- Space-InvaderV1.1/Variable.h
+++ Space-InvaderV1.1/Variable.h
@@ -4,16 +4,19 @@ int t = 500;
 bool solo = false;
 bool start = false;
 
-int xStick;
-int yStick;
-uint32_t gametime;
+int servoAngle = 0;
+int xStick = 0;
+int yStick = 0;
+uint32_t menutime;
 
 int buttonState;
+int buttonPrestate;
 uint32_t systime;
 byte i = 0;             //Variable für Durchlauf des Buchstabenarray`s
-byte j = 0;             //Variable für Durchlauf des Buchstabenarray`s
+byte z = 0;             //Variable für Durchlauf des Buchstabenarray`s
+byte j = 0;
 
-char row1[16];
+char row1[] = "~Press Stick to confirm~";
 char row2[16];
 char arrayLetters[26] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', "S", "T", "U", "V", "W", "X", "Y", "Z"};
 String arrayName[10];
